19f23e89557c85b49ae7350db74d3cb78705216b
NatanAdi <natan.adi@mail.huji.ac.il>
2021-04-13 20:08:23 +0300

    ex3 first commit -oncreate  all view was found

--- app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
+++ app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
     - initial update main text-view based on calculator's output
     - set click listeners on all buttons to operate on the calculator and refresh main text-view
      */
+
+    // finding all views
+    // the main text view.
+    TextView calOutput = findViewById(R.id.textViewCalculatorOutput);
+
+    // the numbers buttons of the calculator.
+    TextView button0 = findViewById(R.id.button0);
+    TextView button1 = findViewById(R.id.button1);
+    TextView button2 = findViewById(R.id.button2);
+    TextView button3 = findViewById(R.id.button3);
+    TextView button4 = findViewById(R.id.button4);
+    TextView button5 = findViewById(R.id.button5);
+    TextView button6 = findViewById(R.id.button6);
+    TextView button7 = findViewById(R.id.button7);
+    TextView button8 = findViewById(R.id.button8);
+    TextView button9 = findViewById(R.id.button9);
+
+    // the operators buttons.
+    TextView plusButton = findViewById(R.id.buttonPlus);
+    TextView minusButton = findViewById(R.id.buttonMinus);
+    TextView equalButton = findViewById(R.id.buttonEquals);
+
+    // delete and clear buttons.
+    View deleteButton = findViewById(R.id.buttonBackSpace);
+    TextView clearButton = findViewById(R.id.buttonClear);
+
+
   }
 
   @Override
